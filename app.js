020b096b471c7dc1e524693003c62d12e10f2158
Amartya Aishwarya <amartya970914@gmail.com>
2018-12-19 21:45:45 +0530

    last commit

--- app.js
+++ app.js
@@ -4,13 +4,22 @@ var request = require("request");
 
 app.set("view engine", "ejs");
 
+app.get("/", function(req,res){
+    res.render("search");
+});
+
 app.get("/results", function(req,res){
     //res.send("Hello, listen");
-    request("http://www.omdbapi.com/?s=iowa&apikey=thewdb", function(error, response, body){
+    var query = req.query.search;
+    var url = "http://www.omdbapi.com/?s=" + query + "&apikey=thewdb";
+    request(url, function(error, response, body){
       if(!error && response.statusCode == 200){
           var data = JSON.parse(body);
           //res.send(results["Search"][0]["Title"]);
           res.render("results", {data: data});
+      }
+      else{
+          console.log("Error");
       }
     });
 });
